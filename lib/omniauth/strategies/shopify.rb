fffa3657ff95d4eae583e4bb715ac967f8d7166a
Kareem Janoudi <kjanoudi@gmail.com>
2015-09-09 10:00:30 -0400

    updated shopify gem

--- lib/omniauth/strategies/shopify.rb
+++ lib/omniauth/strategies/shopify.rb
@@ -5,27 +5,22 @@ module OmniAuth
     class Shopify < OmniAuth::Strategies::OAuth2
       # Available scopes: content themes products customers orders script_tags shipping
       # read_*  or write_*
-      DEFAULT_SCOPE = 'read_products'
+      # DEFAULT_SCOPE = 'read_products'
       MINUTE = 60
       CODE_EXPIRES_AFTER = 10 * MINUTE
 
       option :client_options, {
+        :site => '',
         :authorize_url => '/admin/oauth/authorize',
         :token_url => '/admin/oauth/access_token'
       }
 
       option :callback_url
-      option :myshopify_domain, 'myshopify.com'
-
-      option :setup, proc { |env|
-        request = Rack::Request.new(env)
-        env['omniauth.strategy'].options[:client_options][:site] = "https://#{request.GET['shop']}"
-      }
 
       uid { URI.parse(options[:client_options][:site]).host }
 
       def valid_site?
-        !!(/\A(https|http)\:\/\/[a-zA-Z0-9][a-zA-Z0-9\-]*\.#{Regexp.quote(options[:myshopify_domain])}[\/]?\z/ =~ options[:client_options][:site])
+        !!(/\A(https|http)\:\/\/[a-zA-Z0-9][a-zA-Z0-9\-]*\.myshopify.com[\/]?\z/ =~ options[:client_options][:site])
       end
 
       def valid_signature?
@@ -54,37 +49,36 @@ module OmniAuth
       end
 
       def fix_https
-        options[:client_options][:site].gsub!(/\Ahttp\:/, 'https:')
+        url = options[:client_options][:site]
+        return unless url
+        url.gsub!('http://', 'https://')
+        url = "https://#{url}" unless url.starts_with?('https')
+        options[:client_options][:site] = url
       end
 
       def setup_phase
-        super
+        options[:client_options][:site] = request.GET['shop']
         fix_https
+        super
       end
 
       def request_phase
-        if valid_site?
-          super
-        else
-          fail!(:invalid_site)
-        end
+        byebug
+        fix_https
+        super
       end
 
       def callback_phase
+        fix_https
         return fail!(:invalid_site) unless valid_site?
         return fail!(:invalid_signature) unless valid_signature?
         super
       end
 
-      def authorize_params
-        super.tap do |params|
-          params[:scope] ||= DEFAULT_SCOPE
-        end
-      end
-
       def callback_url
         options[:callback_url] || super
       end
+
     end
   end
 end
